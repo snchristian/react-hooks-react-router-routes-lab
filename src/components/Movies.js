2451f4c013c03d0800fb0ffb6c5f84c10bf74672
Shantel <snchristian8@gmail.com>
2021-12-06 19:31:45 -0500

    commit tree 5e962dc27d0696158117d779584277cbdfcd258f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => <div key={movie.title} >
+      
+      <h2>{movie.title}</h2>
+      <p>Time:{movie.time}</p>
+      <ul>
+        {movie.genres.map(genres => <li key={genres}>
+          {genres}
+        </li>)}
+      </ul>
+    </div>)}
+    </div>;
 }
 
 export default Movies;
